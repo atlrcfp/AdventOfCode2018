1a8352383a0a7c8beaf3c907546c90002b542098
atlrcfp <38670615+atlrcfp@users.noreply.github.com>
2019-02-01 10:50:33 +0000

    Add files via upload

--- day3_part2.py
+++ day3_part2.py
@@ -8,10 +8,11 @@ Created on Thu Jan 31 10:24:12 2019
 import re
 import numpy as np
 
-fabric = np.zeros((1000,1000), dtype = int)
+fabric = np.zeros((1000,1000), dtype = np.int8)
 intersections = 0
 max_width = []
 max_height = []
+this_one = 0
 
 with open('day3.txt', 'r') as file:
     for line in file: #line = file.readline()
@@ -33,13 +34,27 @@ with open('day3.txt', 'r') as file:
                     #this_claim = 1
                     #print(intersections)
         #fabric[y:y+height,x:x+width] = np.ones((height,width), dtype = bool)
+       
+print("Fabric marked out")
+with open('day3.txt', 'r') as file:    
+    for line in file: #line = file.readline()
+        elements = re.split(' |,|: |x|\n', line)
+        #print(elements)
+        x = int(elements[2]) #x starting co-ord, from left
+        y = int(elements[3]) #y starting co-rd, from top
+        width = int(elements[4]) #width of section
+        height = int(elements[5]) #height of section
+        
     
-    for i in range(1000):
-            for j in range(1000):
-                if fabric[i,j] > 1:
-                    intersections += 1
-    print("Total number of common squares of fabric:", intersections)
-    print("Width of fabric,",max(max_width))
-    print("Height of fabric,",max(max_height))
+        this_one = 1
+        
+        for i in range(height):
+            for j in range(width):
+                if fabric[y+i+1,x+j+1] != 1:
+                    this_one = 0
+        
+        if this_one == 1:
+            print(elements)
+    
     
     
